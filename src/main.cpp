315460e13e9ad0b9d0e440f80c7b657bed741e85
viniciushgn <v188182@dac.unicamp.br>
2021-01-31 17:38:37 -0300

    Setup para compilação com GLAD e GLFW realizado

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,56 @@
-#include <iostream>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+
+
+void framebuffer_size_callback(GLFWwindow* window, int largura, int altura);
+
 int main()
 {
+//GLFW INICIALIZANDO------------------------------------------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); OSX
     
-    return 0;
+    GLFWwindow* janela = glfwCreateWindow(800,600,"Learning", NULL, NULL);
+
+    if(janela == NULL){
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(janela);
+    glfwSetFramebufferSizeCallback(janela, framebuffer_size_callback);
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0,0,800,600);
+
+
+//--------------------------------------------------------------------------------------
+//LOOP----------------------------------------------------------------------------------
+while(!glfwWindowShouldClose(janela)){
+    glfwSwapBuffers(janela);
+    glfwPollEvents();
 }
+//--------------------------------------------------------------------------------------
+//TERMINATE-----------------------------------------------------------------------------
+glfwTerminate();
+return 0;
+//--------------------------------------------------------------------------------------
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int largura, int altura)
+{
+    glViewport(0,0,largura,altura);
+}
+
